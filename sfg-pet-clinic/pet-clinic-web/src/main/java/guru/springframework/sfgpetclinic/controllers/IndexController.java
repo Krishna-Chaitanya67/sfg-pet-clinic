384f2f49c931767542f583bab8999e9644bd3798
Krishna-Chaitanya67 <chaitanyak673@gmail.com>
2021-04-26 13:35:53 +0530

    Implemented genric error page.Closes #45

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class IndexController implements ErrorController {
 
     @RequestMapping({"/", "","/index","index.html"})
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/error")
+    public String myerror() {
+        return "notImplemented";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
