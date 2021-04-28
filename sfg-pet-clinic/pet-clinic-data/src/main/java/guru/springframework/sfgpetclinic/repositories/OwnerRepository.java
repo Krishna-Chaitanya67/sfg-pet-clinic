f8ce1406f792f760a408a757ec2efc00b26c0cc7
Krishna-Chaitanya67 <chaitanyak673@gmail.com>
2021-04-28 16:07:11 +0530

    Adding repositories. Closes #39

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.Model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+}
