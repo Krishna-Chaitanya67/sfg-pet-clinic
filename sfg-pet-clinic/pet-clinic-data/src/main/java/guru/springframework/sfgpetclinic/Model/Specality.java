56f89c35556e1997614de41a3d4f51e9a94bfddb
Krishna-Chaitanya67 <chaitanyak673@gmail.com>
2021-04-28 12:42:20 +0530

    Implemented vets entity.Closes #37

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Specality.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Specality.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specalities")
 public class Specality extends BaseEntity{
 
+    @Column(name = "description")
     private String Description;
 
     public String getDescription() {
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Vet.java
@@ -1,10 +1,15 @@
 package guru.springframework.sfgpetclinic.Model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specalities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specality_id"))
     private Set<Specality> specalities = new HashSet<>();
 
     public Set<Specality> getSpecalities() {
