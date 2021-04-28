fb63e1de4f60b657cf7ce1f81e598118f21cbc1e
Krishna-Chaitanya67 <chaitanyak673@gmail.com>
2021-04-28 11:17:33 +0530

    Implemented Owner entity.Closes #36

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Model/Owner.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.Model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @Column(name = "")
     private Set<Pet> pets = new HashSet<>();
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
     public String getAddress() {
